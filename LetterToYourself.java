b1c492e508bc0adc41971c7e05c1a3fd491d1cc4
Nick Azua <nazua@zappos.com>
2014-07-21 12:51:45 -0700

    Finished Ex. 2 and 3

---
+++ LetterToYourself.java
@@ -0,0 +1,17 @@
+public class LetterToYourself
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "+­­­­­­­­­­­­­­-­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­­+" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                    #### |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "|                                Nick Azua                |" );
+		System.out.println( "|                                222 InTech Rd            |" );
+		System.out.println( "|                                Las Vegas, NV 89119      |" );
+		System.out.println( "|                                                         |" );
+		System.out.println( "+---------------------------------------------------------+" );
+	}
+}
