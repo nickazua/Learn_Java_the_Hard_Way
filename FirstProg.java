1a080d0de0464158219f795a1f727375e63e7ea5
Nick Azua <nazua@zappos.com>
2014-07-21 12:08:45 -0700

    Finished exercise 1

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "I, Nick Azua, am determined to learn how to code." );
+		System.out.println( "Today's date is Monday, July 21, 2014");
+	}
+}
